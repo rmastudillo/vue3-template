eb5a69514511fff4baeffed2712503dfa3fc7abc
richi <rmastudillo@uc.cl>
2023-10-24 00:53:03 -0300

    vite clean

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { env } from 'node:process'
 
-console.log(env.URL, env.url)
+console.log(env.URL, env.url, process.env)
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
