d1700ce580abe8e8119bdb0e5f69d2d4977c9740
richi <rmastudillo@uc.cl>
2023-10-24 01:40:12 -0300

    test

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig, loadEnv } from 'vite'
 
 const env = loadEnv(process.cwd(), '')
 
+console.log(import.meta.url)
 export default defineConfig({
   plugins: [vue()],
   base: env.VITE_REPO_NAME ?? '/vue3-test/',
