6aa7a456623b409204fa37b11b75732175a9f5ee
richi <rmastudillo@uc.cl>
2023-10-24 02:17:03 -0300

    123123

--- vite.config.mjs
+++ vite.config.mjs
@@ -3,8 +3,8 @@ import vue from '@vitejs/plugin-vue'
 import { fileURLToPath, URL } from 'node:url'
 import { defineConfig, loadEnv } from 'vite'
 
-console.log(import.meta.url, 'HOLAMUNDOOOOO',  env.VITE_REPO_NAME)
 const env = loadEnv(process.cwd(), '')
+console.log(import.meta.url, 'HOLAMUNDOOOOO',  env.VITE_REPO_NAME)
 
 export default defineConfig({
   plugins: [vue()],
