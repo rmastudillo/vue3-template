c3349d97150175d2cbe892cfc214270bcb069ece
richi <rmastudillo@uc.cl>
2023-10-24 01:03:22 -0300

    test3

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,12 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { env } from 'node:process'
 
-console.log("holamundio")
+console.log('holamundio')
 console.log(env.URL, env.url, process.env)
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: env.VITE_REPO_NAME ?? '/vue3-template/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
