7d30ad50b511693d1fbf435a92e40de461769416
Luis González <ljgonzalez@uc.cl>
2023-10-31 17:56:41 -0300

    Modo producción y modo desarrollo en un script mágico: `./environment-selector-deployment.sh`

---
+++ environment-selector-deployment.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+#alias docker="sudo docker"
+
+source ./.env
+
+compose_method() {
+	local FILE="${1}"
+	local ACTION="${2}"
+	
+	docker compose -f "${FILE}" "${ACTION}"
+}
+
+compose_down() {
+	compose_method ./compose.yml down
+	compose_method ./compose.production.yml down
+}
+
+compose_launch() {
+	local FILE="${1}"
+
+	compose_method "${FILE}" pull
+	compose_method "${FILE}" build
+	docker compose -f "${FILE}" up -d
+}
+
+compose_relaunch() {
+	local FILE="${1}"
+
+	compose_down
+	compose_launch "${FILE}"
+}
+
+
+if [[ "${ENVIRONMENT}" == "production" ]] ; then
+	echo "Producción"
+	COMPOSE_FILE="./compose.production.yml"
+
+else
+	echo "Development"
+	COMPOSE_FILE="./compose.yml"
+fi
+
+compose_relaunch "${COMPOSE_FILE}"
