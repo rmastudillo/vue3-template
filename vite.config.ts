14916a2cffd510b5572dbc97b593e216d208b081
richi <rmastudillo@uc.cl>
2023-11-01 15:30:33 -0300

    hola mundo removed

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,8 @@
-/* eslint-disable no-undef */
 import vue from '@vitejs/plugin-vue'
 import { fileURLToPath, URL } from 'node:url'
 import { defineConfig, loadEnv } from 'vite'
 
 const env = loadEnv(process.cwd(), '')
-console.log(import.meta.url, 'HOLAMUNDOOOOO', env.VITE_REPO_NAME)
 
 export default defineConfig({
   plugins: [vue()],
